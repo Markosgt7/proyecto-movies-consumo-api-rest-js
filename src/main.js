82a152f8366fa9110a0ca9e932abac43f925e7c0
Markosgt7 <markos.guate7@gmail.com>
2022-06-18 20:51:17 -0600

    config del entorno de desarrollo de mi sistema de movies

---
+++ src/main.js
@@ -0,0 +1,2 @@
+
+console.log("API URL=https://www.ñlkjañskjñlkañ/klñaslkjfñalskf?api_key="+ API_KEY)
---
+++ src/secrets.example.js
@@ -0,0 +1,2 @@
+//Crear por favor un archivo secrets.js con tu API KEY
+const API_KEY='ÑLKJAÑSDLKAFJÑD'
